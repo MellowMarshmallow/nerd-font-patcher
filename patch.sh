fcdf58e132cb07f4f0a9d5320261fe7b7aa5ba9d
Mellow Marshmallow <64708999+MellowMarshmallow@users.noreply.github.com>
2021-09-26 16:21:47 -0600

    Add flags

--- patch.sh
+++ patch.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-./FontForge.AppImage -script $PWD/font-patcher $PWD/"$1" -out $PWD/out
+./FontForge.AppImage \
+  -script $PWD/font-patcher $PWD/"$1" \
+    --use-single-width-glyphs \
+    --adjust-line-height \
+    --quiet \
+    --windows \
+    --complete \
+    -out $PWD/out
